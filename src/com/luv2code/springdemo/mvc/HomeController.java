acdc5c0059a98496b36d229ce18048a4b2f14127
wuyeelok <wuyeelok@gmail.com>
2019-07-16 10:59:12 +0800

    Refactor: Specified to use get method

--- src/com/luv2code/springdemo/mvc/HomeController.java
+++ src/com/luv2code/springdemo/mvc/HomeController.java
@@ -2,11 +2,12 @@ package com.luv2code.springdemo.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
+	@RequestMapping(path = "/", method = RequestMethod.GET)
 	public String showPage() {
 		return "main-menu";
 	}
