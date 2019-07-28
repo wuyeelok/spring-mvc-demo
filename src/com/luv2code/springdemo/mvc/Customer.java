f8a1f2101de0e6b5e180a2a61bce1a43d964ded1
wuyeelok <wuyeelok@gmail.com>
2019-07-28 16:44:05 +0800

    Trim user input string in from submit

--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -1,12 +1,14 @@
 package com.luv2code.springdemo.mvc;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public class Customer {
 
 	private String firstName;
 
-	@NotBlank(message = "is required")
+	@NotNull(message = "can not be blank")
+	@Size(min = 1, message = "is required")
 	private String lastName;
 
 	public String getFirstName() {
--- src/com/luv2code/springdemo/mvc/CustomerController.java
+++ src/com/luv2code/springdemo/mvc/CustomerController.java
@@ -2,9 +2,12 @@ package com.luv2code.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +15,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+	// add an initbinder ... to convert trim strings
+	// remove leading annotation
+	// resolve issue for our validation
+
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 
 	@RequestMapping(path = "/showForm", method = RequestMethod.GET)
 	public String showForm(Model theModel) {
